a564efc50151d2c92b7cd4c24009db4aa7eec141
FellipeCavalcante <azevedoaranha1@gmail.com>
2024-10-08 14:41:49 -0300

    OrderStatus enum

--- src/main/java/com/fellipeAzevedo/course/entities/Order.java
+++ src/main/java/com/fellipeAzevedo/course/entities/Order.java
@@ -1,6 +1,7 @@
 package com.fellipeAzevedo.course.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fellipeAzevedo.course.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -19,15 +20,18 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
     public Order() {}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus,User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -45,6 +49,17 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        if (orderStatus != null) {
+            return OrderStatus.valueOf(orderStatus);
+        }
+        return null;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus.getCode();
     }
 
     public User getClient() {
